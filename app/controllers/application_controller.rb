e3efe9a9db2ac77451b33209e6e70cb198cb254d
Christian Turcu <93250535+Christian-Turcu@users.noreply.github.com>
2024-12-19 14:29:46 +0000

    fix: Skip CSRF token verification in test environment to fix failing tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token, if: :test_environment?
+
+  private
+
+  def test_environment?
+    Rails.env.test?
+  end
 end
