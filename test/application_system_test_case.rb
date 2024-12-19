60466723bdd369f50137b13a74c142873bba6d94
Christian Turcu <93250535+Christian-Turcu@users.noreply.github.com>
2024-12-19 15:34:17 +0000

    fix: Update system tests and CI workflow for Chrome compatibility

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  if ENV["CI"]
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |options|
+      options.add_argument("--headless=new")
+      options.add_argument("--no-sandbox")
+      options.add_argument("--disable-dev-shm-usage")
+    end
+  else
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  end
 end
