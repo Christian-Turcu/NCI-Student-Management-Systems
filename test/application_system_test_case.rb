73a5a6db02be026f52abeeaab691cef1498b6825
Christian Turcu <93250535+Christian-Turcu@users.noreply.github.com>
2024-12-19 15:17:36 +0000

    fix: Configure system tests to use Selenium standalone Chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
+    browser: :remote,
+    url: "http://localhost:4444/wd/hub",
+    desired_capabilities: :chrome
+  }
 
   def accept_confirm
     # Accept the Turbo confirmation dialog
