4316dfc34e50faaef61a19d598b1a980f3b4bad6
Christian Turcu <93250535+Christian-Turcu@users.noreply.github.com>
2024-12-19 15:21:49 +0000

    fix: Update system tests to use only Chrome options

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,14 +1,16 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  chrome_options = Selenium::WebDriver::Chrome::Options.new
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-gpu')
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
     browser: :remote,
     url: "http://localhost:4444/wd/hub",
-    capabilities: [
-      Selenium::WebDriver::Chrome::Options.new(
-        args: %w[headless disable-gpu no-sandbox]
-      )
-    ]
+    options: chrome_options
   }
 
   def accept_confirm
